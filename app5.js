8d662d45a3ab8e3065e9733bdb32c0f8c4bb901f
Philip Loa <boyet007@gmail.com>
2018-07-25 13:50:13 +0700

    watch

---
+++ app5.js
@@ -0,0 +1,19 @@
+new Vue  ({
+    el: '#app',
+    data: {
+        kilometers: 0,
+        meters: 0
+    },
+    methods: {},
+    computed: {},
+    watch:{
+        kilometers: function(val) {
+            this.kilometers = val;
+            this.meters = val * 1000;
+        },
+        meters: function(val) {
+            this.kilometers = val / 1000;
+            this.meters = val;
+        }
+    }
+})
