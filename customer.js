cac5ee29c40e1ff7ebfcdad15f6aa14a697aa11a
Philip Loa <boyet007@gmail.com>
2018-07-27 10:55:18 +0700

    customer detail

---
+++ customer.js
@@ -0,0 +1,66 @@
+Vue.component('customercomponent', {
+    template: 
+    '<div class= "Table">' +
+        '<div class="Row" v-bind:style ="styleobj">' +
+            '<div class="Cell">' +
+                '<p>{{ itr.fname }}</p>' +
+            '</div>' +
+            '<div class="Cell">' + 
+                '<p>{{ itr.lname }}</p>' + 
+            '</div>' +
+            '<div class="Cell">' + 
+                '<p>{{ itr.addr }}</p>' + 
+            '</div>' + 
+            '<div class="Cell">' + 
+                '<p><button v-on:click="$emit(\'removeelement\')">x</button></p>' + 
+            '</div>' + 
+        '</div>' + 
+    '</div>',
+    props: ['itr', 'index'],
+    data: function() {
+        return {
+            styleobj : {
+                backgroundColor: this.getcolor(),
+                fontsize: 20
+            }
+        }
+    },
+    methods: {
+        getcolor: function() {
+            if (this.index % 2) {
+                return '#FFE633';
+            } else {
+                return '#D4CA87';
+            }
+        }
+    }
+});
+
+var vm = new Vue ({
+    el: '#databinding',
+    data: {
+        fname: '',
+        lname: '',
+        addr: '',
+        custdet: [],
+        styleobj: {
+            backgroundColor: '#2196F3!important',
+            cursor: 'pointer',
+            padding: '8px 16px',
+            verticalAlign: 'middle'
+        }
+    },
+    methods: {
+        showdata: function() {
+            this.custdet.push({
+                fname: this.fname,
+                lname: this.lname,
+                addr: this.addr
+            });
+            console.log(this.custdet);
+            this.fname = "",
+            this.lname = "",
+            this.addr = "";
+        }
+    }
+});
